9e1577dd50f25bbd0b7bbed0f07ae0086db242b6
Sude Melek Acar <sudeacar86@gmail.com>
2024-12-27 16:28:54 +0300

    son6

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,9 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 };
-
 app.use(allowCrossDomain);
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use("/api",apiRoute);
 
 app.use('/users', usersRouter);

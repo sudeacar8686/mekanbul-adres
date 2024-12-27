b0cb0a950b29515e6fe4fd8f1c9e8e1c1d34f913
Sude Melek Acar <sudeacar86@gmail.com>
2024-12-27 18:13:27 +0300

    son10

--- backend/app_api/models/db.js
+++ backend/app_api/models/db.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 //Cloud mongodb için aşağıdaki adresi cloud adresiyle değiştirin.
-var dbURI="mongodb+srv://sudeacar86:05509192@cluster0.necps.mongodb.net/mekanbul?retryWrites=true&w=majority"
+var dbURI="mongodb+srv://sudeacar86:345@cluster1.gt1kp.mongodb.net/mekanbul2?retryWrites=true&w=majority"
 //var dbURI="mongodb://localhost/mekanbul";
 mongoose.connect(dbURI);
 mongoose.connection.on("connected",function(){

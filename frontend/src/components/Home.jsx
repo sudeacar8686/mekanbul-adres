843171df728903b12f41f8ad7278c76debe9bf13
Sude Melek Acar <sudeacar86@gmail.com>
2024-12-27 15:58:31 +0300

    son2

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,56 +1,56 @@
 import InputWithLabel from "./InputWithLabel";
 import VenueList from "./VenueList";
 import Header from "./Header";
-import React, { useState } from "react";
-import VenueDataService from "../services/VenueDataService"
-import {useSelector,useDispatch} from "react-redux";
-import { useParams } from "react-router-dom";
-
-
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import VenueDataService from "../services/VenueDataService";
 const Home = () => {
-  const dispatch=useDispatch();
-  const venues=useSelector((state) => state.data);
-  const isError=useSelector((state) =>state.isError);
-  const isLoading =useSelector((state) =>state.isLoading);
-  const isSuccess = useSelector((state) =>state.isSuccess);
-  const [coordinate,setCoordinate] = useState({ lat:1, long:1});
-  
+  const [coordinate, setCoordinate] = React.useState({
+    lat: 1,
+    long: 1,
+  });
+  const dispatch = useDispatch();
+  const venues = useSelector((state) => state.data);
+  const isError = useSelector((state) => state.isError);
+  const isLoading = useSelector((state) => state.isLoading);
+  const isSuccess = useSelector((state) => state.isSuccess);
 
-
-  const [searchVenue,setSearchVenue]=useState("");
-  const search=(event)=>{
-    setSearchVenue(event.target.value);
-  };
-  const filteredVenues = Array.isArray(venues) ? venues.filter(
-(venue) =>
-  venue.name.toLowerCase().includes(searchVenue.toLowerCase()) ||
-  venue.address.toLowerCase().includes(searchVenue.toLowerCase())
-  ) : [];
-
-
-   //Şimdilik veri statik. Backend bitince Rest API'den gelecek.
-
-   React.useEffect(() =>{
-    if("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position){
+  React.useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
         setCoordinate({
-          lat:position.coords.latitude,
-          long:position.coords.longitude,
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
         });
       });
     }
-   },[]);
-  
-  React.useEffect(()=>{
-    dispatch({type:"FETCH_INIT"});
-    VenueDataService.nearbyVenues(coordinate.lat,coordinate.long)
-    .then(function(response) {
-      dispatch({type:"FETCH_SUCCESS",payload:response.data});
-    }).catch(function(){
-      dispatch({type:"FETCH_FAILURE"});
-    })},[coordinate.lat,coordinate.long] );
-  
+  }, []);
+
+  React.useEffect(() => {
+    dispatch({ type: "FETCH_INIT" });
+    VenueDataService.nearbyVenues(coordinate.lat, coordinate.long)
+      .then(function (response) {
+        dispatch({ type: "FETCH_SUCCESS", payload: response.data });
+      })
+      .catch(function () {
+        dispatch({ type: "FETCH_FAILURE" });
+      });
+  }, [coordinate.lat, coordinate.long]);
+  //üstteki konumu sürekli güncellesin diye var
+
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+
+  const filteredVenues = venues.filter((venue) =>
+    venue.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+
   return (
     <div>
       <Header
@@ -62,23 +62,18 @@ const Home = () => {
         label="Mekan Ara:"
         type="text"
         isFocused
-        onInputChange={search}
-        value={searchVenue}
+        onInputChange={handleSearchInputChange}
+        value={searchTerm}
       />
       <hr />
       <div className="row">
-        {isError ?(
-          <p>Hata var .</p>
-        ):isLoading?(
-          <p>Mekanlar yükleniyor..</p>
-        ):(
-          isSuccess && (
-          <div className="row">
-           <VenueList venues={filteredVenues}/>  
-           </div> 
-          )
+        {isError ? (
+          <p>Hata var.</p>
+        ) : isLoading ? (
+          <p>Mekanlar Yükleniyor...</p>
+        ) : (
+          isSuccess && <VenueList venues={filteredVenues} />
         )}
-
       </div>
     </div>
   );

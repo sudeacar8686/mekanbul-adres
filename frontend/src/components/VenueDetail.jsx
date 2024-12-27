fe305381e70ddba3c1dbda5c6ad9982fe30936e7
Sude Melek Acar <sudeacar86@gmail.com>
2024-12-27 16:14:18 +0300

    son 4

--- frontend/src/components/VenueDetail.jsx
+++ frontend/src/components/VenueDetail.jsx
@@ -6,19 +6,24 @@ import HourList from "./HourList";
 import CommentList from "./CommentList";
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import VenueDataService from "../services/VenueDataService";
+import VenueDataService from "../services/VenueDataService"
+import {useSelector,useDispatch} from "react-redux";
 
 const VenueDetail = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const venue = useSelector((state) => state.data);
-  React.useEffect(() => {
-    VenueDataService.getVenue(id).then((response) => {
-      dispatch({ type: "FETCH_SUCCESS", payload: response.data });
+  
+  React.useEffect(()=>{
+    dispatch({type:"FETCH_INIT"});
+    VenueDataService.getVenue(id)
+    .then((response) => {
+      dispatch({type:"FETCH_SUCCESS",payload:response.data});
+    }).catch(() => {
+      dispatch({type:"FETCH_FAILURE"});
     });
   }, [id]);
-
+ 
   return (
     <div>
       <Header headerText={venue.name} />
@@ -57,9 +62,11 @@ const VenueDetail = () => {
                     <img
                       className="img img-responsive img-rounded"
                       alt="Konum Bilgisi"
-                      src={`https://maps.googleapis.com/maps/api/staticmap?center=${venue.coordinates
-                        }&zoom=12&size=600x400&markers=${venue.coordinates
-                        }&key=AIzaSyCmmKygTpBzHGOZEciJpAdxC9v_tWHagnE`}
+                      src={`https://maps.googleapis.com/maps/api/staticmap?center=${
+                        venue.coordinates
+                      }&zoom=12&size=600x400&markers=${
+                        venue.coordinates
+                      }&key=AIzaSyCmmKygTpBzHGOZEciJpAdxC9v_tWHagnE`}
                     />
                   </div>
                 </div>
@@ -72,14 +79,14 @@ const VenueDetail = () => {
                 <div className="panel-heading ">
                   <NavLink
                     className="btn btn-default pull-right"
-                    to={/venue/${id}/comment/new} state={{ name: venue.name }}
+                    to={`/venue/${id}/comment/new`}state={{ name: venue.name }}
                   >
                     Yorum Ekle{" "}
                   </NavLink>
                   <h2 className="panel-title ">Yorumlar</h2>
                 </div>
                 <div className="panel-body ">
-                  <CommentList commentList={venue.comments || []} />
+                  <CommentList commentList={venue.comments || [] }/>
                 </div>
               </div>
             </div>
